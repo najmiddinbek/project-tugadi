5d4df86cbe0e22569af5d70f4c3e56d7ed96fb0e
najmiddin <turgunpolatov3gmail.com>
2023-11-09 19:59:58 +0500

    commited

---
+++ models/topic.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const topicSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    school: String,
+    telNumber: Number,
+    darsQoldirish: Number,
+    manzili: String,
+    newSinfi: String,
+    newDarsQoldirish: String,
+    telephoneRaqami: String,
+    newIsm: String,
+    MFY: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
+
+export default Topic;
