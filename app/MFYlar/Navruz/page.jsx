8d01e53da8212c637b66a5747ba50df86bd49bc4
najmiddin <turgunpolatov3gmail.com>
2023-11-09 20:05:59 +0500

    commit

--- app/MFYlar/Navruz/page.jsx
+++ app/MFYlar/Navruz/page.jsx
@@ -3,8 +3,8 @@
 import React, { useState, useEffect } from 'react';
 import { BsChevronDown, BsChevronUp } from "react-icons/bs"
 import Link from "next/link"
-import Navbar from '../../components/Navbar';
-import RemoveBtn from '../../components/RemoveBtn';
+import Navbar from '../../../components/Navbar';
+import RemoveBtn from '../../../components/RemoveBtn';
 
 const getTopics = async () => {
     try {
